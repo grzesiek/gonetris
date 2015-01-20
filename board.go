c304d6f9c17e67838a675b6662389964b4e277e0
Grzegorz Bizon <grzegorz.bizon@ntsn.pl>
2015-01-20 20:37:07 +0100

    Add baord's frame drawing and constructor

--- board.go
+++ board.go
@@ -6,19 +6,48 @@ import (
 )
 
 type Board struct {
-	Runes    [22][22]rune
+	Matrix   [20][20]rune
 	Position Position
 }
 
 func (b *Board) Draw() {
 
-	for row, runes := range b.Runes {
-		for col, _ := range runes {
+	for row, cells := range b.Matrix {
+		for col, _ := range cells {
 			x, y := b.Position.X+row, b.Position.Y+col
 			termbox.SetCell(x, y, '#', termbox.ColorRed, termbox.ColorGreen)
 		}
 	}
 
+}
+
+func (b *Board) DrawFrame() {
+
+	width, height := len(b.Matrix), len(b.Matrix[0])
+	x, y := b.Position.X, b.Position.Y
+	for i := -1; i <= width; i++ {
+		ch := '-'
+		if i == -1 || i == width {
+			ch = '+'
+		}
+		termbox.SetCell(x+i, y-1, ch, termbox.ColorWhite, termbox.ColorBlack)
+		termbox.SetCell(x+i, y+height, ch, termbox.ColorWhite, termbox.ColorBlack)
+	}
+	for i := 0; i < height; i++ {
+		termbox.SetCell(x-1, y+i, '|', termbox.ColorWhite, termbox.ColorBlack)
+		termbox.SetCell(x+width, y+i, '|', termbox.ColorWhite, termbox.ColorBlack)
+	}
+
+}
+
+func NewBoard(x, y int) *Board {
+
+	var board Board
+
+	board.Position = Position{X: x, Y: y}
+	board.DrawFrame()
+
+	return &board
 }
 
 func HandleBoards() {
