9f46f75802d851d03208aaf2fe1cc7e408e3ac9c
Grzegorz Bizon <grzegorz.bizon@ntsn.pl>
2015-01-20 20:36:05 +0100

    Add player struct

---
+++ player.go
@@ -0,0 +1,25 @@
+package main
+
+import ()
+
+type Player struct {
+	Board    *Board
+	Nickname string
+	Host     string
+}
+
+var (
+	Players []*Player
+)
+
+func init() {
+	Players = make([]*Player, 0, opts.Players)
+}
+
+func NewPlayer() *Player {
+
+	var player Player
+	player.Board = NewBoard(5, 5)
+
+	return &player
+}
