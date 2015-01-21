a069268123caeddbeb758faeae33b1b5292dc492
Grzegorz Bizon <grzegorz.bizon@ntsn.pl>
2015-01-21 22:10:36 +0100

    Add brick definitions

--- brick.go
+++ brick.go
@@ -1,12 +1,13 @@
 package main
 
-import ()
+import (
+	"github.com/nsf/termbox-go"
+)
 
-type Brick interface {
-	Draw()
-	MoveLeft()
-	MoveRight()
-	Drop()
+type Brick struct {
+	Position Position
+	Layout   [][]int
+	Color    termbox.Attribute
 }
 
 var (
@@ -15,4 +16,72 @@ var (
 )
 
 func init() {
+
+	IBrick := Brick{
+		Color: termbox.ColorBlue,
+		Layout: [][]int{
+			{1},
+			{1},
+			{1},
+			{1}}}
+
+	JBrick := Brick{
+		Color: termbox.ColorCyan,
+		Layout: [][]int{
+			{1, 1, 1},
+			{0, 0, 1}}}
+
+	LBrick := Brick{
+		Color: termbox.ColorYellow,
+		Layout: [][]int{
+			{1, 1, 1},
+			{1, 0, 0}}}
+
+	OBrick := Brick{
+		Color: termbox.ColorMagenta,
+		Layout: [][]int{
+			{1, 1},
+			{1, 1}}}
+
+	SBrick := Brick{
+		Color: termbox.ColorGreen,
+		Layout: [][]int{
+			{0, 1, 1},
+			{1, 1, 0}}}
+
+	TBrick := Brick{
+		Color: termbox.ColorWhite,
+		Layout: [][]int{
+			{1, 1, 1},
+			{0, 1, 0}}}
+
+	ZBrick := Brick{
+		Color: termbox.ColorRed,
+		Layout: [][]int{
+			{1, 1, 0},
+			{0, 1, 1}}}
+
+	Bricks[0] = IBrick
+	Bricks[1] = JBrick
+	Bricks[2] = LBrick
+	Bricks[3] = OBrick
+	Bricks[4] = SBrick
+	Bricks[5] = TBrick
+	Bricks[6] = ZBrick
+
+}
+
+func (b *Brick) Draw() {
+}
+
+func (b *Brick) MoveLeft() {
+}
+
+func (b *Brick) MoveRight() {
+}
+
+func (b *Brick) Drop() {
+}
+
+func NextBrick() {
 }
