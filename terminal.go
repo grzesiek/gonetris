198b9d4138e142de8e46e30a8be8000ffff430c3
Grzegorz Bizon <grzegorz.bizon@ntsn.pl>
2015-03-21 10:27:28 +0100

    Terminal refactoring (bb)

--- terminal.go
+++ terminal.go
@@ -130,3 +130,34 @@ func (terminal *Terminal) Handle(boardEvent, newBoardEvent chan Board) {
 		termbox.Flush()
 	}
 }
+
+func (terminal *Terminal) HandleKeys(gameCloseEvent chan bool, brickOperationEvent chan string) {
+
+	defer Wg.Done()
+
+	for {
+		if event := termbox.PollEvent(); event.Type == termbox.EventKey {
+
+			switch event.Ch {
+			case 'p': /*	Pause  					 */
+			case 'q': /*	Quit						 */
+				gameCloseEvent <- true
+				return
+			case 'j': /*	Move brick left */
+				brickOperationEvent <- "BrickMoveLeft"
+			case 'l': /*	Move brick right */
+				brickOperationEvent <- "BrickMoveRight"
+			case 'k': /*  Rotate brick */
+				brickOperationEvent <- "BrickRotate"
+			case 'm': /*  Move down brick */
+				brickOperationEvent <- "BrickMoveDown"
+			}
+
+			switch event.Key {
+			case termbox.KeySpace: /*  Drop brick */
+				brickOperationEvent <- "BrickDrop"
+			}
+
+		}
+	}
+}
